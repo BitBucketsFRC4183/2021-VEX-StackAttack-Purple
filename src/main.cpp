90c9feda590ddbcc923390e1c75b2491ede9e217
zak <zagladney@gmail.com>
2021-04-16 17:53:55 -0700

    added soph's new auton code, changed driveSpeed

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,11 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include <iostream>
 
 using namespace vex;
 
-double driveSpeed = 37.5;
+double driveSpeed = 50;
 
 void moveForward() {
   if (Controller1.Axis3.position() > 0) {
@@ -105,16 +106,23 @@ void nudgeRightIntake() {
 double rotateMultiplier = 1.07;
 
 void testAuton() {
-  Drivetrain.driveFor(forward, 36, inches);
+  // Start facing Trash can drive to Trash can and drop off cube then walk the
+  // dog and return home for dinner
+  Drivetrain.driveFor(forward, 34, inches);
   Drivetrain.turnFor(left, 45, degrees);
-  Drivetrain.driveFor(forward, 5, inches);
-  Drivetrain.turnFor(right, 40, degrees);
-  Drivetrain.driveFor(forward, 2, inches);
+  Drivetrain.driveFor(forward, 8, inches);
+  IntakeDown();
+  IntakeDown();
+  IntakeDown();
+  wait(1, seconds);
+  std::cout << "Done waiting" << std::endl;
   Drivetrain.driveFor(forward, -15, inches);
+  Drivetrain.turnFor(right, 40, degrees);
+  Drivetrain.driveFor(forward, 1, inches);
   Drivetrain.turnFor(right, 90, degrees);
-  Drivetrain.driveFor(forward, 22, inches);
-  Drivetrain.turnFor(right, 110, degrees);
-  Drivetrain.driveFor(forward, 32, inches);
+  Drivetrain.driveFor(forward, 20, inches);
+  Drivetrain.turnFor(right, 120, degrees);
+  Drivetrain.driveFor(forward, 31, inches);
   Drivetrain.turnFor(right, 110, degrees);
   Drivetrain.driveFor(forward, 26, inches);
 }
@@ -123,10 +131,10 @@ int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
-  testAuton();
-  Drivetrain.setTurnVelocity(50, percent);
+  Drivetrain.setTurnVelocity(driveSpeed, percent);
+  Drivetrain.setDriveVelocity(driveSpeed, percent);
 
-  Drivetrain.setDriveVelocity(50, percent);
+  testAuton();
 
   Controller1.Axis1.changed(turnRobot);
   Controller1.Axis3.changed(moveForward);
